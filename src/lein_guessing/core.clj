5e71b0054b340814dbf9c7423f5d6fced9ab9b5c
colinl <solartruth101@gmail.com>
2020-12-31 11:15:54 -0500

    better code

--- src/lein_guessing/core.clj
+++ src/lein_guessing/core.clj
@@ -1,5 +1,12 @@
 (ns lein-guessing.core)
 
+
+(def gotWrong)
+(def ranNum)
+(def won)
+
+(declare play-again)
+
 (defn -main
   []
   (def gotWrong 0)
@@ -8,29 +15,29 @@
   (println "Guess a random number, from 0 to to 10!")
 
   (while (not won)
-    (def guess (Integer/parseInt (read-line)))
+    (let [guess (Integer/parseInt (read-line))]
+               (cond
+                 (< guess ranNum) (do
+                                    (println "That is incorrect! Guess higher!")
+                                    (def gotWrong (+ gotWrong 1)))
+                 (> guess ranNum) (do
+                                    (println "That is incorrect! Guess lower!")
+                                    (def gotWrong (+ gotWrong 1)))
+                 :else (do
+                         (if (> gotWrong 3)
+                           (println "You guessed correctly! You got" gotWrong "wrong. You lose.")
+                           (println "You guessed correctly! You got" gotWrong "wrong. You win."))
+                         (def won true)))))
+  (play-again))
 
-    (cond
-      (< guess ranNum) (do
-                         (println "That is incorrect! Guess higher!")
-                         (def gotWrong (+ gotWrong 1)))
-      (> guess ranNum) (do
-                         (println "That is incorrect! Guess lower!")
-                         (def gotWrong (+ gotWrong 1)))
-      :else (do
-              (if (> gotWrong 3)
-                (println "You guessed correctly! You got" gotWrong "wrong. You lose.")
-                (println "You guessed correctly! You got" gotWrong "wrong. You win."))
-              (def won true))))
 
-  (println "Would you like to play again?")
-  (defn play-again
-    []
-    (def a (read-line))
+(defn play-again
+  []
+  (println "Would you like to play again?[y/n]")
+  (let [a (read-line)]
     (cond
       (= a "y") (-main)
       (= a "n") nil
       :else (do
               (println "That is not a valid answer.")
-              (play-again))))
-  (play-again))
+              (play-again)))))
